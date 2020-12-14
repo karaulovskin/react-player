04401ad2ee1c77fea609d383e1220e7030a80edf
Karaulovskin <karaulovskin@yandex.ru>
2020-12-14 18:28:01 +0300

    add constructor Audio

--- src/hoc/Player/Player.js
+++ src/hoc/Player/Player.js
@@ -1,17 +1,22 @@
 import React, {Component} from "react";
 import classes from "../Player/Player.module.css";
-import audio from "../Player/audio/Mathame.mp3";
+import track from "../Player/audio/Mathame.mp3";
 import Timeline from "../../components/Timeline/Timeline";
 import Controls from "../../components/Controls/Controls";
 
+const audio = new Audio();
+audio.src = track;
+
 class Player extends Component {
+
+    componentDidMount() {
+        console.log(audio);
+        audio.play();
+    }
+
     render() {
         return (
             <div className={classes.Player}>
-                <audio
-                    src={audio}
-                    controls
-                />
                 <Timeline />
                 <Controls />
             </div>
