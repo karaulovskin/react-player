ddd9027038a337793845ce18e534727487970f6d
Karaulovskin <karaulovskin@yandex.ru>
2021-02-21 13:55:22 +0300

    написание тестов ошибка модуля rootDir

---
+++ src/redux/messageReducer.test.js
@@ -0,0 +1,20 @@
+import messageReducer, {addMessage} from "./messageReducer";
+
+
+if ('length of message should be incremented', () => {
+    // 1. test data
+    let state = {
+        message: [
+            {id: 1, message: 'Messages 1'},
+            {id: 2, message: 'Messages 2'},
+            {id: 3, message: 'Messages 3'}
+        ],
+    }
+    let action = addMessage('react-player');
+
+    // 2. action
+    let newState = messageReducer(state, action);
+
+    // 3. expectation
+    expect(newState.message.length).toBe(4);
+});
